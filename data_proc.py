2e80270268438b24bc6e7aa8c21e56beada23c3a
Kolten Pearson <kolten@m8int01.fsl.byu.edu>
2016-12-29 15:41:36 -0700

    added data normalization to default dataset

--- data_proc.py
+++ data_proc.py
@@ -5,21 +5,21 @@ import idx2numpy
 import h5py
 import os
 
-def get_category_map(source) :
-    result = {}
-    count = 0
+def get_category_list(source) :
+    result_list = []
 
     for s in source :
-        
-        if (s not in result.keys()) :
-            result[s] = count
-            count += 1
+        if (s not in result_list) :
+            result_list.append(s)
 
-    return result
+    return result_list
 
-#right now uses dtype of uint8, giving an upper limit of 256 labels
-def to_one_hot(source, cat_map) :
-    categories = cat_map
+def to_one_hot(source, cat_list) :
+
+    categories = {}
+    for i, c in enumerate(cat_list) :
+        categories[c] = i
+
     dims = source.shape
     result = np.zeros((dims[0], len(categories.keys())), dtype=np.uint8)
     
@@ -28,6 +28,21 @@ def to_one_hot(source, cat_map) :
         result[i][categories[s]] = 1
 
     return result
+
+def get_norms(dataset) :
+
+    max = np.amax(dataset)
+    min = np.amin(dataset)
+
+    return (min, max)
+
+def get_mean(dataset) :
+    return np.mean(dataset, dtype=np.float64)
+
+def shuffle_data(images, labels) :
+    indices = np.shuffle(np.arange(len(images)))
+
+    return images[indices], labels[indices]
 
 
 def get_mnist() :
@@ -42,11 +57,11 @@ def get_mnist() :
     test_images = idx2numpy.convert_from_file(test_images)
     test_labels = idx2numpy.convert_from_file(test_labels)
 
-    train_images = train_images.reshape([-1, 28, 28, 1])/256
-    test_images = test_images.reshape([-1, 28, 28, 1])/256
-    cat_map = get_category_map(test_labels)
-    train_labels = to_one_hot(train_labels, cat_map)
-    test_labels = to_one_hot(test_labels, cat_map)
+    train_images = train_images.reshape([-1, 28, 28, 1])
+    test_images = test_images.reshape([-1, 28, 28, 1])
+    cat_list = get_category_list(test_labels)
+    train_labels = to_one_hot(train_labels, cat_list)
+    test_labels = to_one_hot(test_labels, cat_list)
 
     return (train_images, train_labels, test_images, test_labels)
 
@@ -62,8 +77,17 @@ def validate_mnist() :
 def make_mnist_h5py(loc) :
     dat = h5py.File(loc, 'w')
     train_image, train_label, test_image, test_label = get_mnist()
+    cat_list = get_category_list(test_labels)
+    min, max = get_norms(train_image)
+    mean = get_mean(train_image)
 
-    out_train_image = dat.create_dataset('train_image', data=train_image, dtype=np.float16)
-    out_train_label = dat.create_dataset('train_label',  data=train_label, dtype=np.float16)
-    out_test_image = dat.create_dataset('test_image', data=test_image, dtype=np.float16)
-    out_test_label = dat.create_dataset('test_label', data=test_label, dtype=np.float16)
+    cat_list = [str(l) for l in cat_list]
+
+    out_train_image = dat.create_dataset('train_image', data=train_image, dtype=np.uint8)
+    out_train_label = dat.create_dataset('train_label',  data=train_label, dtype=np.uint8)
+    out_test_image = dat.create_dataset('test_image', data=test_image, dtype=np.uint8)
+    out_test_label = dat.create_dataset('test_label', data=test_label, dtype=np.uint8)
+    out_label_info = dat.create_dataset('label_info', data=cat_list, dtype=h5py.special_dtype(vlen=unicode))
+    out_max = dat.create_dataset('max', data=max, dtype=np.float64)
+    out_max = dat.create_dataset('min', data=min, dtype=np.float64)
+    out_max = dat.create_dataset('mean', data=mean, dtype=np.float64)
